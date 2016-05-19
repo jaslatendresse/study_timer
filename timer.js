c8e52c7acd01cf1d59b5a4265684c686a4e1950a
jaslatendresse <jlatendresse.39@gmail.com>
2016-05-19 10:23:30 -0400

    Changed layout

--- timer.js
+++ timer.js
@@ -19,6 +19,8 @@ function start(){
         clicked = true;
         document.getElementById('studying').style.display = 'block';
         document.getElementById('pausing').style.display = 'none';
+        document.getElementById('animatepause').style.display = 'none';
+        document.getElementById('stopmsg').innerHTML = '';
     }
     else if(clicked === true){};
 }
@@ -43,8 +45,10 @@ function pause(){
     timer2 = setInterval('setTimePause()',1000);
     document.getElementById('studying').style.display = 'none';
     document.getElementById('pausing').style.display = 'block';
+    document.getElementById('animatepause').style.display = 'block';
     timer = null;
     clicked = false;
+    document.getElementById('stopmsg').innerHTML = '';
 }
 
 function stop(){
@@ -56,9 +60,9 @@ function stop(){
     
     document.getElementById('studying').style.display = 'none';
     document.getElementById('pausing').style.display = 'none';
-    document.getElementById('stopmsg').innerHTML = 'You have studied for ' + hours + 'Hhrs ' + minutes + 'Mins ' + secs + 'Secs';
-    document.getElementById('pausemsg').innerHTML = 'Your break was ' + hours2 + 'Hhrs ' + minutes2 + 'Mins ' + secs2 + 'Secs long'; 
+    document.getElementById('stopmsg').innerHTML = '<b>Study time</b>: ' + hours + ' hours, ' + minutes + ' minutes and ' + secs + ' seconds.<br>' + '<b>Pause time</b>: ' + hours2 + ' hours, ' + minutes2 + ' minutes and ' + secs2 + ' seconds.';
     document.getElementById('timer2').style.display = 'none';
+    document.getElementById('animatepause').style.display = 'none';
 }
 
 function reset(){
@@ -72,10 +76,10 @@ function reset(){
     document.getElementById('minutes').innerHTML = '0' + minutes;
     document.getElementById('hours').innerHTML = '0' + hours;
     document.getElementById('stopmsg').innerHTML = '';
-    document.getElementById('pausemsg').innerHTML = '';
     document.getElementById('timer2').style.display = 'none';
     document.getElementById('studying').style.display = 'none';
     document.getElementById('pausing').style.display = 'none';
+    document.getElementById('animatepause').style.display = 'none';
     clicked = false;
 }
 
@@ -92,4 +96,34 @@ function setTime(){
     document.getElementById('seconds').innerHTML = (secs<10)?'0'+secs:secs;
     document.getElementById('minutes').innerHTML = (minutes<10)?'0'+minutes:minutes;
     document.getElementById('hours').innerHTML = (hours<10)?'0'+hours:hours;
+}
+
+function displayDate() {
+    var d = new Date(),
+        year = d.getFullYear(),
+        months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    document.getElementById('date').innerHTML = months[d.getMonth()] + ' ' + d.getDate() + ', ' + year;
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function () {
+        startTime()
+    }, 500);
 }
